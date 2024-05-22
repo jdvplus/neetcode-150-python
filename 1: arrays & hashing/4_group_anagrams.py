8f9e3c3ac2813fdc2eb38d8918881e89047e9594
Jaime de Venecia <deveneciaj@gmail.com>
2024-05-21 23:45:01 -0700

    update test cases, add readme

--- 1: arrays & hashing/4_group_anagrams.py
+++ 1: arrays & hashing/4_group_anagrams.py
@@ -54,7 +54,7 @@ def group_anagrams(strs: List[str]) -> List[List[str]]:
   
   return res.values()
 
-print(group_anagrams(["eat","tea","tan","ate","nat","bat"]))
+print(group_anagrams(['eat','tea','tan','ate','nat','bat']))
   # should log: [["bat"],["nat","tan"],["ate","eat","tea"]]
 print(group_anagrams([''])) # should log: [['']]
 print(group_anagrams(['a'])) # should log: [['a']]
---
+++ README.md
@@ -0,0 +1,6 @@
+# neetcode 150
+
+- optimized, annotated solutions in python
+- includes time & space complexity considerations
+- tested with leetcode
+- source: https://neetcode.io/practice
